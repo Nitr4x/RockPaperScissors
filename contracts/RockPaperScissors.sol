7faaf81678c6e2331b36f699cf577857585d9ac5
nitrax <jean.lejeune@protonmail.com>
2019-08-05 20:28:29 +0200

    Updating contract

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -8,68 +8,28 @@ contract RockPaperScissors is Pausable {
 
     enum Moves {NONE, ROCK, PAPER, SCISSORS}
 
-    struct Player {
-        bytes32 hashedMove;
-        Moves move;
-        uint bet;
-        bool isPlaying;
-    }
-    
+    uint constant minDeadline = 5 minutes;
+
     struct Game {
         uint deadline;
         uint timelimit;
         uint bet;
-        address player1;
-        address player2;
+        address opponent;
+        Moves opponentMove;
     }
     
-    mapping(address => Game) public _games;
-    mapping(address => Player) public _players;
+    mapping(bytes32 => Game) public games;
+    mapping(address => uint) public balances;
 
-    event LogMovePlaced(address indexed player, bytes32 move);
+    event LogMovePlaced(bytes32 indexed session, address player, Moves move);
     event LogPlayerEnrolled(address indexed player);
-    event LogGameResolved(address indexed winner, address loser);
-    event LogGameCancelled(address indexed player);
+    event LogGameResolved(bytes32 indexed session, address winner, address loser);
+    event LogGameCancelled(bytes32 indexed session, address player);
     event LogBetWithdrawed(address indexed player, uint amount);
-    event LogPlayerPenalized(address indexed player);
-    event LogMoveRevealed(address indexed player);
-    event LogGameCreated(address indexed game, address player1, address player2);
-    event LogGameDestroyed(address indexed player, address game);
+    event LogPlayerPenalized(bytes32 indexed session, address player);
+    event LogGameCreated(bytes32 indexed session, address player1, address player2);
+    event LogGameDestroyed(bytes32 indexed session, address player);
 
-    modifier _onlyParticipant(address session) {
-        Game memory game = _games[session];
-
-        require((msg.sender == game.player1) || (msg.sender == game.player2), "You don't participe to this game");
-        _;
-    }
-
-    modifier _isNotResolved(address session) {
-        Game memory game = _games[session];
-
-        require(game.timelimit < now && game.timelimit != 0, "Game is active or already resolved");
-        _;
-    }
-
-    modifier _isResolved(address session) {
-        Game memory game = _games[session];
-
-        require(game.timelimit < now && game.timelimit == 0, "Game is active or already resolved");
-        _;
-    }
-
-    modifier _isActive(address session) {
-        require(_games[session].timelimit > now, "Game is over");
-        _;
-    }
-
-    modifier _isReadyToDestroy(address session) {
-         Game memory game = _games[session];
-
-        require(game.timelimit == 0, "Game is not over");
-        require(_players[game.player1].bet == 0 && _players[game.player2].bet == 0, "A player has not withdrawed his bet");
-        _;
-    }
-    
     function hashMove(address player, bytes32 nonce, Moves move) public view returns(bytes32 hash) {
         require(player != address(0x0), "Invalid address");
         require(nonce != 0 && move != Moves.NONE, "Incorrect nonce or move");
@@ -77,132 +37,100 @@ contract RockPaperScissors is Pausable {
         hash = keccak256(abi.encodePacked(address(this), player, move, nonce));
     }
 
-    function create(address player1, address player2, uint deadline, uint bet) public whenNotPaused returns(bool success) {
-        require(player1 != address(0x0) && player2 != address(0x0), "Player incorrect");
-        require(_games[player1].deadline != 0, "The creator already did not finish and/or destroy the previous game");
-        require(!_players[player1].isPlaying && !_players[player2].isPlaying, "Player not available");
-        require(deadline > 5 minutes, "Deadline too short");
+    function create(bytes32 hashedMove, address opponent, uint deadline, uint bet) public whenNotPaused payable returns(bool success) {
+        require(msg.value != bet, "The ether amount sent and the bet are not equal");
+        require(opponent != address(0x0) && hashedMove != 0, "Opponent or move incorrect");
+        require(games[hashedMove].deadline != 0, "This game already exist");
+        require(deadline > minDeadline, "Deadline too short");
 
-        emit LogGameCreated(player1, player1, player2);
+        emit LogGameCreated(hashedMove, msg.sender, opponent);
 
-        _games[player1].deadline = deadline;
-        _games[player1].timelimit = now.add(deadline);
-        _games[player1].bet = bet; 
-        _games[player1].player1 = player1;
-        _games[player2].player2 = player2;
+        games[hashedMove].deadline = deadline;
+        games[hashedMove].timelimit = now.add(deadline);
+        games[hashedMove].bet = bet; 
+        games[hashedMove].opponent = opponent;
 
         return true;
     }
 
-    function placeMove(address session, bytes32 hash) public _onlyParticipant(session) _isActive(session) whenNotPaused payable returns(bool success) {
-        Game memory game = _games[session];
+    function placeMove(bytes32 session, Moves move) public whenNotPaused payable returns(bool success) {
+        Game memory game = games[session];
 
+        require(game.timelimit > now && game.bet != 0, "Game is over");
+        require(game.opponent == msg.sender, "You don't participate to this game");
         require(msg.value == game.bet, "Wrong bet");
-        require(_players[msg.sender].hashedMove == 0, "You already played");
-        require(hash != 0, "Your move is incorrect");
+        require(move != Moves.NONE, "Wrong move");
+        require(game.opponentMove == Moves.NONE, "You already played");
         
-        emit LogMovePlaced(msg.sender, hash);
+        emit LogMovePlaced(session, msg.sender, move);
         
-        _players[msg.sender].hashedMove = hash;
-        _players[msg.sender].bet = _players[msg.sender].bet.add(msg.value);
-        _games[session].timelimit = now.add(game.deadline);
+        games[session].opponentMove = move;
+        games[session].timelimit = now.add(game.deadline);
 
         return true;
     }
-        
-    function revealMove(address session, bytes32 nonce, Moves move) public _onlyParticipant(session) _isActive(session) whenNotPaused returns(bool success) {
-        require(_players[msg.sender].hashedMove != 0, "The opponent has not played yet");
 
-        bytes32 hash = hashMove(msg.sender, nonce, move);
-        
-        require(_players[msg.sender].hashedMove == hash, "Wrong nonce and/or move");
-        
-        emit LogMoveRevealed(msg.sender);
-        
-        _players[msg.sender].move = move;
-        _games[session].timelimit = now.add(_games[session].deadline);
+    function resolve(bytes32 session, bytes32 nonce, Moves move) public whenNotPaused returns(bool success) {        
+        require(session == hashMove(msg.sender, nonce, move), "Wrong nonce and/or move. Note: only the game creator can resolve the game");
 
-        return true;
-    }
-    
-    function resolve(address session) public _onlyParticipant(session) returns(bool success) {
-        Game memory game = _games[session];
+        Game memory game = games[session];
 
-        Moves MPlayer1 = _players[game.player1].move;
-        Moves MPlayer2 = _players[game.player2].move;
+        require(game.timelimit > now && game.bet != 0, "Game is over");
+        require(game.opponentMove != Moves.NONE, "The opponent has not played yet");
 
-        require(MPlayer1 != Moves.NONE && MPlayer2 != Moves.NONE, "A player has not revealed his move");
+        address winner = (move == Moves.ROCK && game.opponentMove == Moves.PAPER) ? game.opponent
+            : (move == Moves.PAPER && game.opponentMove == Moves.SCISSORS) ? game.opponent
+            : (move == Moves.SCISSORS && game.opponentMove == Moves.ROCK) ? game.opponent
+            : msg.sender;
+        address loser = (winner == msg.sender) ? game.opponent : msg.sender;
 
-        address winner = (MPlayer1 == Moves.ROCK && MPlayer2 == Moves.PAPER) ? game.player2
-            : (MPlayer1 == Moves.PAPER && MPlayer2 == Moves.SCISSORS) ? game.player2
-            : (MPlayer1 == Moves.SCISSORS && MPlayer2 == Moves.ROCK) ? game.player2
-            : game.player1;
-        address loser = (winner == game.player1) ? game.player2 : game.player1;
-        
-        emit LogGameResolved(winner, loser);
+        emit LogGameResolved(session, winner, loser);
 
-        _players[winner].bet = _players[winner].bet.add(_players[loser].bet);
-        _players[loser].bet = 0;
-        _games[session].timelimit = 0;
+        balances[winner] = balances[winner].add(game.bet * 2);
+        games[session].bet = 0;
 
         return true;
     }
     
-    function cancel(address session) public _onlyParticipant(session) _isNotResolved(session) returns(bool success) {
-        Game memory game = _games[session];
+    function cancel(bytes32 session, bytes32 nonce, Moves move) public returns(bool success) {
+        Game memory game = games[session];
 
-        address opponent = (msg.sender == game.player1) ? game.player2 : game.player1;
+        require(game.timelimit > now && game.bet != 0, "Game is over");
+        require(hashMove(msg.sender, nonce, move) == session && game.opponentMove == Moves.NONE, "You cannot cancel this game");
 
-        require((_players[msg.sender].hashedMove != 0 && _players[opponent].hashedMove == 0)
-            || (_players[msg.sender].hashedMove == 0 && _players[opponent].hashedMove == 0), "You cannot cancel the game");
+        emit LogGameCancelled(session, msg.sender);
 
-        emit LogGameCancelled(msg.sender);
-
-        _games[session].timelimit = 0;
-
-        return true;
-    }
-
-    function penalize(address session) public _onlyParticipant(session) _isNotResolved(session) returns(bool success) {
-        Game memory game = _games[session];
-
-        address opponent = (msg.sender == game.player1) ? game.player2 : game.player1;
-
-        require(_players[msg.sender].move != Moves.NONE && _players[opponent].hashedMove != 0 
-            && _players[opponent].move == Moves.NONE, "You cannot claim a penality");
-
-        emit LogPlayerPenalized(opponent);
-
-        _players[msg.sender].bet = _players[msg.sender].bet.add(_players[opponent].bet);
-        _players[opponent].bet = 0;
-        _games[session].timelimit = 0;
+        balances[msg.sender] = balances[msg.sender].add(game.bet);
+        games[session].bet = 0;
 
         return true;
     }
 
-    function withdraw(address session) public _isResolved(session) returns(bool success) {        
-        uint amount = _players[msg.sender].bet;
+    function penalize(bytes32 session) public returns(bool success) {
+        Game memory game = games[session];
+
+        require(game.timelimit < now && game.bet != 0, "Game is not over yet");
+        require(msg.sender == game.opponent && game.opponentMove != Moves.NONE, "You cannot claim a penality");
+
+        emit LogPlayerPenalized(session, msg.sender);
+
+        balances[msg.sender] = balances[msg.sender].add(game.bet * 2);
+        games[session].bet = 0;
+
+        return true;
+    }
+
+    function withdraw() public returns(bool success) {
+        uint amount = balances[msg.sender];
         
         require(amount > 0, "Nothing to withdraw");
         
-        _players[msg.sender].bet = 0;
+        balances[msg.sender] = 0;
         
         emit LogBetWithdrawed(msg.sender, amount);
         
         msg.sender.transfer(amount);
         
-        return true;
-    }
-
-    function destroy(address session) public _onlyParticipant(session) _isReadyToDestroy(session) returns(bool success) {
-        Game memory game = _games[session];
-
-        emit LogGameDestroyed(msg.sender, session);
-
-        delete(_players[game.player1]);
-        delete(_players[game.player2]);
-        delete(_games[session]);
-
         return true;
     }
 }
